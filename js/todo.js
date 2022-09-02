13bb266a0ac425f928481642e9b0e1237afb41e1
Seu Nome <voce@exemplo.com>
2022-09-02 14:46:16 -0300

    "Functions Delete and Edit(date it's not working)"

--- js/todo.js
+++ js/todo.js
@@ -2,35 +2,51 @@ var app = new Vue({
   el: "#app",
   data: {
     tasks: [],
-    tempTitle: "",
-    tempProject: "",
-    tempDueTo: "",
+    task: {
+      "tempTitle": null,
+      "tempProject": null,
+      "tempDueTo": null,
+    },
+    modoAdicionar: false,
+    modoEditar: false,
   },
   methods: {
     getTasks() {
       fetch("http://localhost:3000/tasks")
         .then((response) => response.json())
         .then((tarefasJson) => {
-          console.log(tarefasJson);
           this.tasks = tarefasJson;
         });
     },
     addTasks() {
-      console.log(this.tempTitle);
-      let data_ = {
+      let data = {
+        title: this.task.tempTitle,
+        project: this.task.tempProject,
+        dueTo: this.task.tempDueTo,
+      };
+      let dataConverted = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          title: this.tempTitle,
-          project: this.tempProject,
-          dueTo: this.tempDueTo,
-        }),
+        body: JSON.stringify(data),
       };
-      fetch("http://localhost:3000/tasks", data_).then(() => {
-        window.location.href = "http://127.0.0.1:5503/tasksList.html";
+      fetch("http://localhost:3000/tasks", dataConverted).then(() => {
+        this.getTasks();
       });
     },
-    editTasks() {},
+    editTasks(tarefaId) {
+      this.modoEditar = true;
+      const tasking = this.tasks.filter((t) => t.id == tarefaId)[0];
+      console.log(tasking)
+      this.task.id = tasking.id
+      this.task.tempTitle = tasking.title
+      this.task.tempProject = tasking.project
+      this.task.tempDueTo = tasking.dueTo
+    },
+    deleteTasks(tarefaId) {
+      axios.delete(`http://localhost:3000/tasks/${tarefaId}`).then(() => {
+        this.getTasks();
+      });
+    },
   },
   created() {
     this.getTasks();
